f7315f6b5007f5e42eb20b2d9fbabf9328cfa354
newjade <knewjade@gmail.com>
2018-03-13 22:58:42 +0900

    Setup minimum hyperapp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-export function hello(): string {
-    return 'hello';
+import { app, h, View } from 'hyperapp';
+
+class State {
+    public constructor(public readonly count: number) {
+    }
 }
 
+class Actions {
+    public down() {
+        return (state: State) => new State(state.count - 1);
+    }
 
-console.log(hello());
+    public up() {
+        return (state: State) => new State(state.count + 1);
+    }
+}
+
+const view: View<State, Actions> = (state, actions) => {
+    return h('main', {}, [
+        h('h1', {}, state.count),
+        h('button', { onclick: actions.down, disabled: state.count <= 0 }, '-'),
+        h('button', { onclick: actions.up }, '+'),
+    ]);
+};
+
+const mainApp = app(new State(0), new Actions(), view, document.body);
+
+// for unittest
+function hello(): string {
+    return 'hello';
+}
