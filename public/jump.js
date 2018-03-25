74302a649917d31acd6109defb98aeaf00967587
newjade <knewjade@gmail.com>
2018-03-25 23:09:39 +0900

    Add description in help

--- public/jump.js
+++ public/jump.js
@@ -1,1 +1,4 @@
-javascript:(function(){encode(1);window.location.href='https://knewjade.github.io/fumen-for-mobile/index.html?d='+encodeURIComponent(document.getElementById('tx').value);})();
+javascript:(function(){encode(1);window.open('https://knewjade.github.io/fumen-for-mobile/index.html?d='+encodeURIComponent(document.getElementById('tx').value), '_blank');})();
+
+
+'a"b" \'ok\' '
