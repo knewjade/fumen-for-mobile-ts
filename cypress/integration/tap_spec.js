86a29daa7246ea5caf5339af1bb19c4bd065b955
newjade <knewjade@gmail.com>
2018-04-13 00:50:34 +0900

    Rearrange files for cypress

--- cypress/integration/tap_spec.js
+++ cypress/integration/tap_spec.js
@@ -1,37 +1,32 @@
+const datatest = value => `[datatest="${value}"]`;
+
 describe('Tap', () => {
+    // タップのテスト
     it('Next / Prev', () => {
-        cy.visit('./public/index.html');
+        cy.visit('./public/index.html?d=v115@vhCRQJUmBKpB');
 
         cy.viewport(375, 667);  // Like iPhone7
 
-        // テト譜を開く
-        cy.get('#btn-open-fumen').click();
+        // Assertion: ページ番号の確認
+        cy.get(datatest('tools')).find(datatest('text-pages')).should('have.text', '1 / 3');
 
-        // テト譜を入力 : 成功
-        cy.get('#textarea-fumen-modal').clear();
-        cy.get('#textarea-fumen-modal').type('v115@vhCRQJUmBKpB');
-        cy.get('#btn-fumen-modal-open').click();
-
-        cy.get('#text-pages').contains('1 / 3');
-        cy.wait(200);
-
-        cy.get(`#ce-block-3-0}`).contains();
+        // ブロックの存在確認
+        // cy.get(`#ce-block-3-0}`).should('exist');
 
         // 右半分をクリック
         cy.get('body').click(300, 300);
-        cy.get('#text-pages').contains('2 / 3');
+        cy.get(datatest('tools')).find(datatest('text-pages')).should('have.text', '2 / 3');
 
         // 右半分をクリック
         cy.get('body').click(300, 300);
-        cy.get('#text-pages').contains('3 / 3');
+        cy.get(datatest('tools')).find(datatest('text-pages')).should('have.text', '3 / 3');
 
         // 左半分をクリック
         cy.get('body').click(100, 300);
-        cy.get('#text-pages').contains('2 / 3');
+        cy.get(datatest('tools')).find(datatest('text-pages')).should('have.text', '2 / 3');
 
         // 左半分をクリック
         cy.get('body').click(100, 300);
-        cy.get('#text-pages').contains('1 / 3');
+        cy.get(datatest('tools')).find(datatest('text-pages')).should('have.text', '1 / 3');
     });
 });
-
