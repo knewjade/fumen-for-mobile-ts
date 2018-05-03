ef3d3b68e31ce62633a6b508493c3189400eb6be
newjade <knewjade@gmail.com>
2018-05-03 16:32:42 +0900

    Add button to copy to clipboard

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,7 +1,8 @@
-import { Component, px, style } from '../lib/types';
+import { Component, ComponentWithText, px, style } from '../lib/types';
 import { h } from 'hyperapp';
 import { resources } from '../states';
 import { i } from '@hyperapp/html';
+import { encode, Page } from '../lib/fumen/fumen';
 
 declare const M: any;
 
@@ -98,12 +99,13 @@ export const OpenFumenModal: Component<OpenFumenModalProps> = ({ textAreaValue, 
 
 interface SettingsProps {
     version: string;
+    pages: Page[];
     actions: {
         closeSettingsModal: () => void;
     };
 }
 
-export const SettingsModal: Component<SettingsProps> = ({ version, actions }) => {
+export const SettingsModal: Component<SettingsProps> = ({ version, pages, actions }) => {
     const oncreate = (element: HTMLDivElement) => {
         const instance = M.Modal.init(element, {
             onOpenEnd: () => {
@@ -130,7 +132,7 @@ export const SettingsModal: Component<SettingsProps> = ({ version, actions }) =>
         resources.modals.settings = undefined;
     };
 
-    const properties = style({
+    const divProperties = style({
         margin: 0,
         padding: 0,
         display: 'flex',
@@ -147,26 +149,74 @@ export const SettingsModal: Component<SettingsProps> = ({ version, actions }) =>
 
                     <h4>Settings <span style={style({ color: '#999', fontSize: '50%' })}>[build {version}]</span></h4>
 
-                    <div style={properties}>
-                        <a href="./help.html">
-                            <i className="material-icons z-depth-1" style={style({
-                                width: px(50),
-                                height: px(40),
-                                lineHeight: px(40),
-                                fontSize: px(31.25),
-                                display: 'block',
-                                color: '#333',
-                                margin: px(5),
-                                border: 'solid 1px #999',
-                                boxSizing: 'border-box',
-                                textAlign: 'center',
-                                cursor: 'pointer',
-                            })}>help_outline</i>
-                            <div style={style({ textAlign: 'center', color: '#333' })}>help</div>
-                        </a>
+                    <div style={divProperties}>
+                        <SettingButton href="#" iconName="content_copy" onclick={async () => {
+                            // テト譜の変換
+                            const data = await encode(pages);
+
+                            // コピー用のelementを作成
+                            const element = document.createElement('pre');
+                            {
+                                const style = element.style;
+                                style.position = 'fixed';
+                                style.left = '-100%';
+                                element.textContent = 'v115@' + data;
+                                document.body.appendChild(element);
+                            }
+
+                            // クリップボードにコピーする
+                            if (element !== undefined && element !== null) {
+                                document.getSelection().selectAllChildren(element);
+                                const success = document.execCommand('copy');
+                                if (!success) {
+                                    console.error('Cannot copy fumen');
+                                }
+                            } else {
+                                console.error('Unexpected element to copy');
+                            }
+
+                            M.toast({ html: 'Copied to clipboard', classes: 'mytoast', displayLength: 600 });
+
+                            // コピー用のelementを削除
+                            document.body.removeChild(element);
+                        }}>clipboard</SettingButton>
+
+                        <SettingButton href="./help.html" iconName="help_outline">help</SettingButton>
+
+                        <div style={style({ height: px(10), width: '100%', backgroundColor: '#fff' })}/>
+
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
+
+interface SettingButtonProps {
+    href?: string;
+    onclick?: () => void;
+    iconName: string;
+}
+
+export const SettingButton: ComponentWithText<SettingButtonProps> = ({ href = '#', onclick, iconName }, showName) => (
+    <a href={href} onclick={onclick}>
+        <i className="material-icons z-depth-1" style={style({
+            width: px(50),
+            height: px(40),
+            lineHeight: px(40),
+            fontSize: px(31.25),
+            display: 'block',
+            color: '#333',
+            margin: px(5),
+            border: 'solid 1px #999',
+            boxSizing: 'border-box',
+            textAlign: 'center',
+            cursor: 'pointer',
+        })}>{iconName}</i>
+
+        <div style={style({ textAlign: 'center', color: '#333' })}>
+            {showName}
+        </div>
+    </a>
+);
