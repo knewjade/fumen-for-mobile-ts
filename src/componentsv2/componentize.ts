77834bbae65a218b9e326c36847612efec3c241d
knewjade <knewjade@gmail.com>
2019-08-09 00:04:45 +0900

    Refactoring

--- src/componentsv2/componentize.ts
+++ src/componentsv2/componentize.ts
@@ -16,8 +16,7 @@ class Hub<Locals> {
     public readonly locals: Locals;
 
     private prevWatchKey_: { [key in string]: Primitive } = {};
-    private watchKey_: { [key in string]: Primitive } = {};
-    private watchs_: { [key in string]: (locals: Locals) => Primitive } = {};
+    private watches_: { [key in string]: (locals: Locals) => Primitive } = {};
 
     private shouldUpdate_ = true;
 
@@ -26,16 +25,17 @@ class Hub<Locals> {
     }
 
     watch(key: string, func: (locals: Locals) => Primitive): void {
-        this.watchs_[key] = func;
+        this.watches_[key] = func;
     }
 
     refresh() {
-        forEach(this.watchs_, (func, key) => {
-            this.watchKey_[key] = func(this.locals);
+        const watchKey: { [key in string]: Primitive } = {};
+        forEach(this.watches_, (func, key) => {
+            watchKey[key] = func(this.locals);
         });
 
-        this.shouldUpdate_ = this.shouldUpdate_ || !isEqual(this.watchKey_, this.prevWatchKey_);
-        this.prevWatchKey_ = { ...this.watchKey_ };
+        this.shouldUpdate_ = this.shouldUpdate_ || !isEqual(watchKey, this.prevWatchKey_);
+        this.prevWatchKey_ = watchKey;
 
         if (this.shouldUpdate_) {
             main.refresh();
@@ -57,7 +57,7 @@ export function componentize<Props, Actions, Locals>(
 ): (props: Props, actions: Actions) => Component<Props> {
     return (initProps, initActions) => {
         const hub = new Hub<Locals>(initLocals);
-        const g = generator(hub, initProps, initActions);
+        const component = generator(hub, initProps, initActions);
 
         let prev: { props: Props, node: VNode<object> | null } = {
             props: { ...initProps },
@@ -67,7 +67,7 @@ export function componentize<Props, Actions, Locals>(
         return {
             render: (props: Props) => {
                 if (prev.node === null || !isEqual(props, prev.props) || hub.shouldUpdate) {
-                    const node = g.render(props, hub.locals);
+                    const node = component.render(props, hub.locals);
                     prev = { node, props: { ...props } };
                     hub.updated();
                     return node;
